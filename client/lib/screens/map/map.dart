9829b4e8f27a04ff31b896fc0d24a3292ad75f11
insd47 <me@insd.dev>
2024-08-28 21:51:05 +0900

    chore: some refactor

--- client/lib/screens/map/map.dart
+++ client/lib/screens/map/map.dart
@@ -1,20 +1,13 @@
-import 'package:heron/screens/map/filter.dart';
 import 'package:heron/screens/map/googlemap.dart';
 import 'package:flutter/material.dart';
 import 'package:heron/screens/map/overlay.dart';
 import 'package:heron/widgets/appbar/status.dart';
-import 'package:heron/widgets/button/button.dart';
-import 'package:heron/widgets/chip/chip.dart';
-import 'package:hugeicons/hugeicons.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final topMargin = MediaQuery.of(context).padding.top;
-    final colorScheme = Theme.of(context).colorScheme;
-
     return StatusBarColor(
       child: Stack(
         children: [
